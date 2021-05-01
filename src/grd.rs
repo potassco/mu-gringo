e051e28b39e5dd8e3f45187847adf1659b56e164
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2021-05-02 01:20:50 +0200

    restructure

--- src/grd.rs
+++ src/grd.rs
@@ -13,9 +13,16 @@ pub fn print_dep_graph<'a>(graph: &'a Vec<Vec<Vec<&'a Rule>>>) {
 }
 
 pub fn grd<'a>(seq: &'a Vec<Vec<Vec<&'a Rule>>>) {
+    use core::cell::Cell;
+
+    let x = Cell::new(42);
+    let y = &x;
+    let z = &x;
+    y.set(41);
+    assert_eq!(z.get(), 41);
+
     print_dep_graph(&seq);
     // - ground rule w.r.t. sets I and J
     // - ground component w.r.t. sets I and J
     // - ground program
-    
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use core::cell::Cell;
-
 #[macro_use] extern crate lalrpop_util;
 
 mod ast;
@@ -11,12 +9,6 @@ use crate::grd::grd;
 use crate::dep::{check, grd_seq, order};
 
 fn main() {
-    let x = Cell::new(42);
-    let y = &x;
-    let z = &x;
-    y.set(41);
-    assert_eq!(z.get(), 41);
-
     let mut prg = parse("p(1). b(X) :- not a(X), p(X). a(X) :- not b(X), p(X).").unwrap();
     assert!(check(prg.iter()));
     order(&mut prg.iter_mut());
